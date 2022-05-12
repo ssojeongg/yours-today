0ece6f350fa11c71533b8d3a02acecf2e6b7244f
ssojeongg <esojeong2@gmail.com>
2022-05-12 21:38:39 +0900

    update

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const loginTitle = document.querySelector(".login-form h1");
+const header = document.querySelector("header");
 
 const USERNAME_KEY = "username";
 
@@ -10,6 +11,7 @@ function showUserinfo() {
   loginTitle.insertAdjacentHTML("afterbegin", "<span></span>");
   const userinfo = document.querySelector(".login-form h1 span");
   userinfo.innerText = `${username}`;
+  showLogoutBtn();
 }
 
 function handleLoginSubmit(event) {
@@ -20,8 +22,30 @@ function handleLoginSubmit(event) {
 }
 loginForm.addEventListener("submit", handleLoginSubmit);
 
-
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername !== null) {
   showUserinfo();
+}
+
+function showLogoutBtn() {
+  const logout = document.createElement("div");
+  header.appendChild(logout);
+  logout.classList.add("logout");
+  const logoutIcon = document.createElement("i");
+  logout.appendChild(logoutIcon);
+  logoutIcon.classList.add("fa");
+  logoutIcon.classList.add("fa-sign-out");
+  logout.addEventListener("click", handleLogoutBtn);
+}
+
+const logoutBtn = document.querySelector(".logout");
+
+function handleLogoutBtn(event) {
+  if (savedUsername !== null) {
+    logoutBtn.style.display = "none";
+    localStorage.removeItem(USERNAME_KEY);
+    const savedUserinfo = document.querySelector(".login-form h1 span");
+    loginTitle.removeChild(savedUserinfo);
+    loginTitle.prepend(loginInput);
+  }
 }
--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,27 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".todo-form input");
 const todoList = document.querySelector(".todo-list");
 
-function showTodo(newTodo) {
+const TO_DOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TO_DOS_KEY, JSON.stringify(toDos));
+}
+
+function showTodo(newToDoObj) {
   const li = document.createElement("li");
+  li.id = newToDoObj.id;
   todoList.appendChild(li);
   const addedTodo = document.createElement("span");
   addedTodo.classList.add("added-todo");
   li.appendChild(addedTodo);
-  addedTodo.innerText = newTodo;
+  addedTodo.innerText = newToDoObj.text;
   const btnCheck = document.createElement("button");
-  btnCheck.classList.add("todo-finish__btn");
+  btnCheck.classList.add("todo__inner");
   li.prepend(btnCheck);
-  const firstSpan = document.createElement("span");
-  btnCheck.appendChild(firstSpan);
-  btnCheck.addEventListener("click", checkTodo);
+  const span = document.createElement("span");
+  btnCheck.appendChild(span);
   const btnDelete = document.createElement("button");
   btnDelete.classList.add("todo-delete__btn");
   li.appendChild(btnDelete);
@@ -23,29 +31,32 @@ function showTodo(newTodo) {
   btnDelete.addEventListener("click", deleteTodo);
 }
 
-function checkTodo(event) {
-  const btnCheck = document.querySelector(".todo-finish__btn span");
-  const addedTodo = document.querySelector(".added-todo");
-  const linedClass = "todo-finish__lined";
-  const checkedClass = "todo-finish__checked";
-  btnCheck.classList.add(checkedClass);
-  addedTodo.classList.add(linedClass);
-}
-
 function deleteTodo(event) {
+  const li = document.querySelector("li");
   const buttonTag = document.querySelector(".todo-delete__btn");
-  const clicked = event.target.parentElement;
-  if (clicked === buttonTag) {
-    clicked.parentElement.remove();
-  }
+  const clicked = event.currentTarget;
+  clicked.parentElement.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
-
 
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  showTodo(newTodo);
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  }
+  toDos.push(newToDoObj);
+  showTodo(newToDoObj);
+  saveToDos();
 }
-
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedToDos = localStorage.getItem(TO_DOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(showTodo);
+}
